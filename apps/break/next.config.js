84976f559b9db92dfd99321079f20df4435d825e
이자훈 <jh.lee@break.co.kr>
2022-10-29 19:27:06 +0900

    test

--- apps/break/next.config.js
+++ apps/break/next.config.js
@@ -1,6 +1,18 @@
-module.exports = {
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const withTM = require("next-transpile-modules")(["break-ui"]);
+
+module.exports = withTM({
   reactStrictMode: true,
   experimental: {
-    transpilePackages: ["ui"],
+    transpilePackages: ["break-ui"],
   },
-};
+  webpack(config, options) {
+    const { dev, isServer } = options;
+    if (dev) {
+      config.plugins.push(
+        new ForkTsCheckerWebpackPlugin()
+      );
+    }
+    return config;
+  },
+});
